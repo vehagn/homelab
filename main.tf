d6a9ce3cbd4a844cb37d2bae62794d2c39f4aeea
Vegard Hagen <vegard@edgeworks.no>
2022-05-29 12:04:44 +0200

    feat(ingress): Provision Traefik as an ingress controller

--- main.tf
+++ main.tf
@@ -27,6 +27,20 @@ resource "helm_release" "cilium" {
 
 }
 
+resource "kubernetes_namespace" "traefik" {
+  metadata {
+    name = "traefik"
+  }
+}
+
+resource "helm_release" "traefik" {
+  name = "traefik"
+
+  repository = "https://helm.traefik.io/traefik"
+  chart = "traefik"
+  namespace = "traefik"
+}
+
 #resource "kubernetes_namespace" "test" {
 #  metadata {
 #    name = "nginx"
