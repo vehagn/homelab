a4cec11c9990b7581d0d7bbd3359f7b0aaca6a91
Vegard Hagen <vegard@edgeworks.no>
2022-05-29 12:08:05 +0200

    fix

--- main.tf
+++ main.tf
@@ -4,12 +4,16 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.5.0"
+    }
   }
 }
 
-#provider "kubernetes" {
-#  config_path = "~/.kube/config"
-#}
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
 
 provider "helm" {
   kubernetes {
