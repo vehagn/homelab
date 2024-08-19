e315ebddd1f81beb45d9d3ba230dabf1caf1d66a
Vegard Hagen <vegard@stonegarden.dev>
2024-08-19 23:00:58 +0200

    chore(docs): update readme

--- tofu/kubernetes/main.tf
+++ tofu/kubernetes/main.tf
@@ -6,7 +6,8 @@ module "talos" {
   }
 
   image = {
-    version        = "v1.8.0-alpha.1"
+    version = "v1.8.0-alpha.1"
+    update_version = "v1.8.0-alpha.1" # renovate: github-releases=siderolabs/talos
     schematic = file("${path.module}/talos/image/schematic.yaml")
   }
 
@@ -53,15 +54,15 @@ module "talos" {
       cpu           = 4
       ram_dedicated = 4096
     }
-#    "work-00" = {
-#      host_node     = "abel"
-#      machine_type  = "worker"
-#      ip            = "192.168.1.110"
-#      mac_address   = "BC:24:11:2E:A8:00"
-#      vm_id         = 810
-#      cpu           = 8
-#      ram_dedicated = 4096
-#    }
+    #    "work-00" = {
+    #      host_node     = "abel"
+    #      machine_type  = "worker"
+    #      ip            = "192.168.1.110"
+    #      mac_address   = "BC:24:11:2E:A8:00"
+    #      vm_id         = 810
+    #      cpu           = 8
+    #      ram_dedicated = 4096
+    #    }
   }
 
 }
