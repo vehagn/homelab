144247cfffbfb45622f9f336135e676926b1e7cb
Karteek <karteekiitg@gmail.com>
2025-02-13 19:41:43 +0530

    Fix for_each error in image download resource by using static schematic hash instead of derived schematic id as id

--- tofu/kubernetes/talos/image.tf
+++ tofu/kubernetes/talos/image.tf
@@ -1,11 +1,18 @@
 locals {
-  version = var.image.version
+  version   = var.image.version
   schematic = var.image.schematic
-  image_id = "${talos_image_factory_schematic.this.id}_${local.version}"
 
-  update_version = coalesce(var.image.update_version, var.image.version)
+  update_version   = coalesce(var.image.update_version, var.image.version)
   update_schematic = coalesce(var.image.update_schematic, var.image.schematic)
-  update_image_id = "${talos_image_factory_schematic.updated.id}_${local.update_version}"
+
+  image_hashed        = "${local.version}_${sha256(local.schematic)}"
+  update_image_hashed = "${local.update_version}_${sha256(local.update_schematic)}"
+
+  image_filename        = "talos-${local.version}-${talos_image_factory_schematic.this.id}-${var.image.platform}-${var.image.arch}.img"
+  update_image_filename = "talos-${local.update_version}-${talos_image_factory_schematic.updated.id}-${var.image.platform}-${var.image.arch}.img"
+
+  image_url        = "${var.image.factory_url}/image/${talos_image_factory_schematic.this.id}/${local.version}/${var.image.platform}-${var.image.arch}.raw.gz"
+  update_image_url = "${var.image.factory_url}/image/${talos_image_factory_schematic.updated.id}/${local.update_version}/${var.image.platform}-${var.image.arch}.raw.gz"
 }
 
 resource "talos_image_factory_schematic" "this" {
@@ -17,14 +24,21 @@ resource "talos_image_factory_schematic" "updated" {
 }
 
 resource "proxmox_virtual_environment_download_file" "this" {
-  for_each = toset(distinct([for k, v in var.nodes : "${v.host_node}_${v.update == true ? local.update_image_id : local.image_id}"]))
+  for_each = {
+    for k, v in var.nodes :
+    "${v.host_node}_${v.update == true ? local.update_image_hashed : local.image_hashed}" => {
+      host_node      = v.host_node
+      image_url      = "${v.update == true ? local.update_image_url : local.image_url}"
+      image_filename = "${v.update == true ? local.update_image_filename : local.image_filename}"
+    }
+  }
 
-  node_name    = split("_", each.key)[0]
+  node_name    = each.value.host_node
   content_type = "iso"
   datastore_id = var.image.proxmox_datastore
 
-  file_name               = "talos-${split("_",each.key)[1]}-${split("_", each.key)[2]}-${var.image.platform}-${var.image.arch}.img"
-  url = "${var.image.factory_url}/image/${split("_", each.key)[1]}/${split("_", each.key)[2]}/${var.image.platform}-${var.image.arch}.raw.gz"
+  file_name               = each.value.image_filename
+  url                     = each.value.image_url
   decompression_algorithm = "gz"
   overwrite               = false
 }
