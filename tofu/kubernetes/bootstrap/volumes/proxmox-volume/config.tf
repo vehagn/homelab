67f160fea1fd2aa7ff2ce3cf59da93fc2ee83b82
Vegard Hagen <vegard@stonegarden.dev>
2025-04-17 18:23:34 +0200

    fix(tofu): volume-provisioning
    A Proxmox update probably broke/changed the way API requests are handled, giving errors on UPDATE calls with parameters that aren't used
    
    Error: unexpected response code '400': {"data":null,"errors":{"format":"property is not defined in schema and the schema does not allow additional properties","vmid":"property is not defined in schema and the schema does not allow additional properties","size":"property is not defined in schema and the schema does not allow additional properties","filename":"property is not defined in schema and the schema does not allow additional properties"}}
    
    Also update Mastercard/restapi provider

--- tofu/kubernetes/bootstrap/volumes/proxmox-volume/config.tf
+++ tofu/kubernetes/bootstrap/volumes/proxmox-volume/config.tf
@@ -3,17 +3,28 @@ locals {
 }
 
 resource "restapi_object" "proxmox-volume" {
-  path = "/api2/json/nodes/${var.volume.node}/storage/${var.volume.storage}/content/"
+  path = "/api2/json/nodes/${var.volume.node}/storage/${var.volume.storage}/content"
 
   id_attribute = "data"
 
-  force_new = [var.volume.size]
+  debug = true
 
   data = jsonencode({
     vmid     = var.volume.vmid
     filename = local.filename
     size     = var.volume.size
     format   = var.volume.format
+  })
+
+  // Proxmox returns a different object which we ignore.
+  // The size is also returned in bytes, not with prefix, e.g. 1G, 512Mi.
+  // Setting to false (default) triggers a change each run
+  ignore_all_server_changes = true
+
+  // Providing a supported parameter that doesn't do anything.
+  // Supplying either `null` or an empty object makes this fall back to the `data` object.
+  update_data = jsonencode({
+    node = var.volume.node
   })
 
   lifecycle {
