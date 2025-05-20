6b8b69f058765b6c501ab612804d6e955515bb10
karteekiitg <120569182+karteekiitg@users.noreply.github.com>
2025-05-20 07:07:55 +0530

    Using devcontainer and infisical cli instead of infisical action which is buggy
    Signed-off-by: Karteek <120569182+karteekiitg@users.noreply.github.com>

--- .devcontainer/setup_infisical.sh
+++ .devcontainer/setup_infisical.sh
@@ -4,6 +4,7 @@ if [[ -v TF_VAR_infisical_domain ]] && [[ -n "$TF_VAR_infisical_domain" ]] &&
    [[ -v TF_VAR_infisical_client_id ]] && [[ -n "$TF_VAR_infisical_client_id" ]] &&
    [[ -v TF_VAR_infisical_project_id ]] && [[ -n "$TF_VAR_infisical_project_id" ]] &&
    [[ -v TF_VAR_infisical_ro_secrets_path ]] && [[ -n "$TF_VAR_infisical_ro_secrets_path" ]] &&
+   [[ -v TF_VAR_infisical_rw_secrets_path ]] && [[ -n "$TF_VAR_infisical_rw_secrets_path" ]] &&
    [[ -v TF_VAR_infisical_client_secret ]] && [[ -n "$TF_VAR_infisical_client_secret" ]]; then
 
   export INFISICAL_TOKEN=$(infisical login --method=universal-auth --domain="$TF_VAR_infisical_domain" --client-id="$TF_VAR_infisical_client_id" --client-secret="$TF_VAR_infisical_client_secret" --silent --plain)
@@ -13,8 +14,15 @@ if [[ -v TF_VAR_infisical_domain ]] && [[ -n "$TF_VAR_infisical_domain" ]] &&
   fi
 
   infisical export --domain="$TF_VAR_infisical_domain" --projectId="$TF_VAR_infisical_project_id" --path="$TF_VAR_infisical_ro_secrets_path" --format=dotenv-export > "$HOME/.infisical_exports.env"
+
   if [ $? -ne 0 ]; then
-    echo -e "\033[31mZsh Warning: Infisical export failed.\033[0m"
+    echo -e "\033[31mZsh Warning: Infisical ro export failed.\033[0m"
+    exit 1
+  fi
+
+  infisical export --domain="$TF_VAR_infisical_domain" --projectId="$TF_VAR_infisical_project_id" --path="$TF_VAR_infisical_rw_secrets_path" --format=dotenv-export >> "$HOME/.infisical_exports.env"
+  if [ $? -ne 0 ]; then
+    echo -e "\033[31mZsh Warning: Infisical rw export failed.\033[0m"
     exit 1
   fi
 
@@ -24,6 +32,7 @@ else
   if [[ ! -v TF_VAR_infisical_client_id ]]; then echo -e "\033[31m  - TF_VAR_infisical_client_id\033[0m"; fi
   if [[ ! -v TF_VAR_infisical_project_id ]]; then echo -e "\033[31m  - TF_VAR_infisical_project_id\033[0m"; fi
   if [[ ! -v TF_VAR_infisical_ro_secrets_path ]]; then echo -e "\033[31m  - TF_VAR_infisical_ro_secrets_path\033[0m"; fi
+  if [[ ! -v TF_VAR_infisical_rw_secrets_path ]]; then echo -e "\033[31m  - TF_VAR_infisical_rw_secrets_path\033[0m"; fi
   if [[ ! -v TF_VAR_infisical_client_secret ]]; then echo -e "\033[31m  - TF_VAR_infisical_client_secret\033[0m"; fi
 fi
 
